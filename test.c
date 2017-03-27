4f456b960327ef22e8cc944d458d271e5b86e8cc
Simon Mutch <smutch@unimelb.edu.au>
2017-03-27 19:51:40 +1100

    Fix test.c to reflect new parameter ordering.

--- test.c
+++ test.c
@@ -9,17 +9,17 @@ int main(int argc, char *argv[])
 
   init_mlog(MPI_COMM_WORLD, stdout, stdout, stderr);
 
-  mlog(MLOG_MESG, "G'day %s", name);
-  mlog(MLOG_OPEN | MLOG_TIMERSTART, "Starting timer 1:");
-  mlog(MLOG_CONT, "This log message won't start a new line...");
-  mlog(MLOG_MESG | MLOG_TIMERSTART, "...Timers can also be nested, so let's start timer 2.");
-  mlog(MLOG_CLOSE | MLOG_TIMERSTOP, "...and end it.  That was quick!");
-  mlog(MLOG_MESG, "Now wait for 1 second.");
+  mlog("G'day %s", MLOG_MESG, name);
+  mlog("Starting timer 1:", MLOG_OPEN | MLOG_TIMERSTART);
+  mlog("This log message won't start a new line...", MLOG_CONT);
+  mlog("...Timers can also be nested, so let's start timer 2.", MLOG_MESG | MLOG_TIMERSTART);
+  mlog("...and end it.  That was quick!", MLOG_CLOSE | MLOG_TIMERSTOP);
+  mlog("Now wait for 1 second.", MLOG_MESG);
   sleep(1);
-  mlog(MLOG_TIMERSTOP | MLOG_CONT, "...and check timer 1...");
-  mlog(MLOG_MESG, "Excellent!");
+  mlog("...and check timer 1...", MLOG_TIMERSTOP | MLOG_CONT);
+  mlog("Excellent!", MLOG_MESG);
 
-  mlog(MLOG_NOINDENT, "<WARNING> An error is coming...");
+  mlog("<WARNING> An error is coming...", MLOG_NOINDENT);
   mlog_error("Don't worry - this is just a test.");
 
   MPI_Finalize();
