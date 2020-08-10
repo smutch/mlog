27a612eaeb67259d329f8abaf9a1b2a342391d52
Simon Mutch <smutch@unimelb.edu.au>
2020-08-10 15:27:10 +1000

    Use clang-tidy and pre-commit package hooks

--- mlog.c
+++ mlog.c
@@ -1,117 +1,105 @@
 #include "mlog.h"
 #include <stdarg.h>
 
-void init_mlog(MPI_Comm comm,
-    FILE* file_info,
-    FILE* file_warning,
-    FILE* file_error)
+void init_mlog(MPI_Comm comm, FILE* file_info, FILE* file_warning, FILE* file_error)
 {
-    for (int ii = 0; ii < MLOG_NTIMERS; ii++)
+  for (int ii = 0; ii < MLOG_NTIMERS; ii++)
+    _mlog.timer_on[ii] = false;
+
+  _mlog.file_info = file_info;
+  _mlog.file_warning = file_warning;
+  _mlog.file_error = file_error;
+
+  _mlog.comm = comm;
+}
+
+void _mlog_error(const char* fmt, const char* file_name, const char* func_name, int lineno, ...)
+{
+  int rank;
+
+  MPI_Comm_rank(_mlog.comm, &rank);
+  fprintf(_mlog.file_error, "<ERROR> rank %d [%s->%s +%d]: ", rank, func_name, file_name, lineno);
+
+  va_list args;
+  va_start(args, lineno);
+  vfprintf(_mlog.file_error, fmt, args);
+  va_end(args);
+
+  fprintf(_mlog.file_error, "\n");
+  fflush(_mlog.file_error);
+}
+
+void _mlog_info(const char* fmt, int flags, const char* file_name, const char* func_name, int lineno, ...)
+{
+  int rank;
+
+  MPI_Comm_rank(_mlog.comm, &rank);
+  FILE* fd = _mlog.file_info;
+
+  if ((rank != 0) && ((flags & MLOG_ALLRANKS) != MLOG_ALLRANKS))
+    return;
+
+  if ((flags & MLOG_CLOSE) == MLOG_CLOSE) {
+    _mlog.indent--;
+    if (_mlog.indent < 0)
+      _mlog.indent = 0;
+  }
+
+  if ((flags & MLOG_CONT) != MLOG_CONT) {
+    fprintf(fd, "\n");
+    if ((flags & MLOG_NOINDENT) != MLOG_NOINDENT)
+      for (int ii = 0; ii < _mlog.indent; ii++)
+        fprintf(fd, "    ");
+  }
+
+  if ((flags & MLOG_ALLRANKS) == MLOG_ALLRANKS)
+    fprintf(fd, "rank %d: ", rank);
+
+  if ((flags & MLOG_LOCATION) == MLOG_LOCATION)
+    fprintf(fd, "[%s->%s +%d] ", func_name, file_name, lineno);
+
+  va_list args;
+  va_start(args, lineno);
+  vfprintf(fd, fmt, args);
+  va_end(args);
+
+  if ((flags & MLOG_TIMERSTOP) == MLOG_TIMERSTOP) {
+    bool success = false;
+    struct timeval now, result;
+
+    gettimeofday(&now, NULL);
+
+    for (int ii = MLOG_NTIMERS - 1; ii >= 0; ii--)
+      if (_mlog.timer_on[ii]) {
+        timersub(&now, &_mlog.timer[ii], &result);
         _mlog.timer_on[ii] = false;
+        success = true;
+        break;
+      }
+    if (success)
+      fprintf(fd, " (%ld.%06ld s)", (long int)result.tv_sec, (long int)result.tv_usec);
+    else
+      mlog_error("Failed to find open timer to close.");
+  }
 
-    _mlog.file_info = file_info;
-    _mlog.file_warning = file_warning;
-    _mlog.file_error = file_error;
+  if ((flags & MLOG_OPEN) == MLOG_OPEN)
+    _mlog.indent++;
 
-    _mlog.comm = comm;
-}
+  if ((flags & MLOG_TIMERSTART) == MLOG_TIMERSTART) {
+    // find the first empty timer and record the current time
+    bool success = false;
+    for (int ii = 0; ii < MLOG_NTIMERS; ii++)
+      if (!_mlog.timer_on[ii]) {
+        gettimeofday(&_mlog.timer[ii], NULL);
+        _mlog.timer_on[ii] = true;
+        success = true;
+        break;
+      }
 
-void _mlog_error(const char* fmt,
-    const char* file_name,
-    const char* func_name,
-    int lineno,
-    ...)
-{
-    int rank;
+    if (!success)
+      mlog_error("Maximum timer depth exceeded.");
+  }
 
-    MPI_Comm_rank(_mlog.comm, &rank);
-    fprintf(_mlog.file_error, "<ERROR> rank %d [%s->%s +%d]: ", rank, func_name, file_name, lineno);
-
-    va_list args;
-    va_start(args, lineno);
-    vfprintf(_mlog.file_error, fmt, args);
-    va_end(args);
-
-    fprintf(_mlog.file_error, "\n");
-    fflush(_mlog.file_error);
-}
-
-void _mlog_info(const char* fmt,
-    int flags,
-    const char* file_name,
-    const char* func_name,
-    int lineno,
-    ...)
-{
-    int rank;
-
-    MPI_Comm_rank(_mlog.comm, &rank);
-    FILE* fd = _mlog.file_info;
-
-    if ((rank != 0) && ((flags & MLOG_ALLRANKS) != MLOG_ALLRANKS))
-        return;
-
-    if ((flags & MLOG_CLOSE) == MLOG_CLOSE) {
-        _mlog.indent--;
-        if (_mlog.indent < 0)
-            _mlog.indent = 0;
-    }
-
-    if ((flags & MLOG_CONT) != MLOG_CONT) {
-        fprintf(fd, "\n");
-        if ((flags & MLOG_NOINDENT) != MLOG_NOINDENT)
-            for (int ii = 0; ii < _mlog.indent; ii++)
-                fprintf(fd, "    ");
-    }
-
-    if ((flags & MLOG_ALLRANKS) == MLOG_ALLRANKS)
-        fprintf(fd, "rank %d: ", rank);
-
-    if ((flags & MLOG_LOCATION) == MLOG_LOCATION)
-        fprintf(fd, "[%s->%s +%d] ", func_name, file_name, lineno);
-
-    va_list args;
-    va_start(args, lineno);
-    vfprintf(fd, fmt, args);
-    va_end(args);
-
-    if ((flags & MLOG_TIMERSTOP) == MLOG_TIMERSTOP) {
-        bool success = false;
-        struct timeval now, result;
-
-        gettimeofday(&now, NULL);
-
-        for (int ii = MLOG_NTIMERS - 1; ii >= 0; ii--)
-            if (_mlog.timer_on[ii]) {
-                timersub(&now, &_mlog.timer[ii], &result);
-                _mlog.timer_on[ii] = false;
-                success = true;
-                break;
-            }
-        if (success)
-            fprintf(fd, " (%ld.%06ld s)", (long int)result.tv_sec, (long int)result.tv_usec);
-        else
-            mlog_error("Failed to find open timer to close.");
-    }
-
-    if ((flags & MLOG_OPEN) == MLOG_OPEN)
-        _mlog.indent++;
-
-    if ((flags & MLOG_TIMERSTART) == MLOG_TIMERSTART) {
-        // find the first empty timer and record the current time
-        bool success = false;
-        for (int ii = 0; ii < MLOG_NTIMERS; ii++)
-            if (!_mlog.timer_on[ii]) {
-                gettimeofday(&_mlog.timer[ii], NULL);
-                _mlog.timer_on[ii] = true;
-                success = true;
-                break;
-            }
-
-        if (!success)
-            mlog_error("Maximum timer depth exceeded.");
-    }
-
-    if ((flags & MLOG_FLUSH) == MLOG_FLUSH)
-        fflush(fd);
+  if ((flags & MLOG_FLUSH) == MLOG_FLUSH)
+    fflush(fd);
 }
--- test.c
+++ test.c
@@ -3,26 +3,26 @@
 
 int main(int argc, char* argv[])
 {
-    MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
 
-    const char* name = "Sport!";
+  const char* name = "Sport!";
 
-    init_mlog(MPI_COMM_WORLD, stdout, stdout, stderr);
+  init_mlog(MPI_COMM_WORLD, stdout, stdout, stderr);
 
-    mlog("G'day %s", MLOG_MESG, name);
-    mlog("Starting timer 1:", MLOG_OPEN | MLOG_TIMERSTART);
-    mlog("This log message won't start a new line...", MLOG_CONT);
-    mlog("...Timers can also be nested, so let's start timer 2.", MLOG_MESG | MLOG_TIMERSTART);
-    mlog("...and end it.  That was quick!", MLOG_CLOSE | MLOG_TIMERSTOP);
-    mlog("Now wait for 1 second.", MLOG_MESG);
-    sleep(1);
-    mlog("...and check timer 1...", MLOG_TIMERSTOP | MLOG_CONT);
-    mlog("Excellent!", MLOG_MESG);
+  mlog("G'day %s", MLOG_MESG, name);
+  mlog("Starting timer 1:", MLOG_OPEN | MLOG_TIMERSTART);
+  mlog("This log message won't start a new line...", MLOG_CONT);
+  mlog("...Timers can also be nested, so let's start timer 2.", MLOG_MESG | MLOG_TIMERSTART);
+  mlog("...and end it.  That was quick!", MLOG_CLOSE | MLOG_TIMERSTOP);
+  mlog("Now wait for 1 second.", MLOG_MESG);
+  sleep(1);
+  mlog("...and check timer 1...", MLOG_TIMERSTOP | MLOG_CONT);
+  mlog("Excellent!", MLOG_MESG);
 
-    mlog("<WARNING> An error is coming...", MLOG_NOINDENT);
-    mlog_error("Don't worry - this is just a test.");
+  mlog("<WARNING> An error is coming...", MLOG_NOINDENT);
+  mlog_error("Don't worry - this is just a test.");
 
-    MPI_Finalize();
+  MPI_Finalize();
 
-    return 0;
+  return 0;
 }
